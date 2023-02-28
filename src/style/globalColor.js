e06ffe4d965117a704ac2049e0f2a9d5d4ee11dc
sweeeeetpotato <sweet_potato13@naver.com>
2023-03-01 04:03:56 +0900

    style : 글로벌컬러 변수명 수정

--- src/style/globalColor.js
+++ src/style/globalColor.js
@@ -1,12 +1,12 @@
 const palette = {
   mainColor: "#21BF48",
   mainTextColor: "#000000",
-  blackTextColor: "#333333",
-  redTextColor: "#EB5757",
-  grayTextColor: "#767676",
-  whiteTextColor: "#FFFFFF",
-  borderColor: "#C4C4C4",
-  backgroundColor: "#F2F2F2",
+  lightblack: "#333333",
+  red: "#EB5757",
+  darkGray: "#767676",
+  gray: "#C4C4C4",
+  lightGray: "#F2F2F2",
+  white: "#FFFFFF",
 };
 
 export default palette;
