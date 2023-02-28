ff2e99a3bcbcb752dcd60de3b58604658786b02e
sweeeeetpotato <sweet_potato13@naver.com>
2023-02-28 22:30:21 +0900

    style : 자주 사용하는 색상 및 스타일 초기화 글로벌 정의

---
+++ src/style/globalColor.js
@@ -0,0 +1,12 @@
+const palette = {
+  mainColor: "#21BF48",
+  mainTextColor: "#000000",
+  blackTextColor: "#333333",
+  redTextColor: "#EB5757",
+  grayTextColor: "#767676",
+  whiteTextColor: "#FFFFFF",
+  borderColor: "#C4C4C4",
+  backgroundColor: "#F2F2F2",
+};
+
+export default palette;
