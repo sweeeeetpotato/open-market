6a563c7164b87516a287322775d95857f2adebe6
sweeeeetpotato <sweet_potato13@naver.com>
2023-03-02 03:14:12 +0900

    style : 다양한 버튼 컴포넌트 UI 구현

---
+++ src/components/atoms/button/index.jsx
@@ -0,0 +1,198 @@
+import React from "react";
+import { Button, TabButton } from "./style";
+
+export function LoginButton({ children, onClick, disabled }) {
+  if (disabled) {
+    return (
+      <Button type='button' onClick={onClick} backColor='gray'>
+        {children}
+      </Button>
+    );
+  } else {
+    return (
+      <Button type='button' onClick={onClick}>
+        {children}
+      </Button>
+    );
+  }
+}
+
+export function SignUpButton({ children, onClick }) {
+  return (
+    <Button
+      type='button'
+      onClick={onClick}
+      width='122px'
+      height='54px'
+      font='16px'
+      weight='500'
+    >
+      {children}
+    </Button>
+  );
+}
+
+export function PurchaseButton({ children, onClick }) {
+  return (
+    <Button type='button' onClick={onClick} width='416px' height='60px'>
+      {children}
+    </Button>
+  );
+}
+
+export function ShoppingBasketButton({ children, onClick }) {
+  return (
+    <Button
+      type='button'
+      onClick={onClick}
+      width='200px'
+      height='60px'
+      backColor='darkGray'
+    >
+      {children}
+    </Button>
+  );
+}
+
+export function ProductTabButton({ children, onClick, disabled }) {
+  if (disabled) {
+    return (
+      <TabButton
+        type='button'
+        onClick={onClick}
+        fontColor='mainColor'
+        border='mainColor'
+      >
+        {children}
+      </TabButton>
+    );
+  } else {
+    return (
+      <TabButton
+        type='button'
+        onClick={onClick}
+        weight='500'
+        fontColor='darkGray'
+        border='gray'
+      >
+        {children}
+      </TabButton>
+    );
+  }
+}
+
+export function ModalButton({ children, onClick, whiteStyle }) {
+  if (whiteStyle) {
+    return (
+      <Button
+        type='button'
+        onClick={onClick}
+        width='100px'
+        height='40px'
+        font='16px'
+        weight='400'
+        backColor='white'
+        fontColor='darkGray'
+        borderColor='gray'
+      >
+        {children}
+      </Button>
+    );
+  } else {
+    return (
+      <Button
+        type='button'
+        onClick={onClick}
+        width='100px'
+        height='40px'
+        font='16px'
+        weight='400'
+      >
+        {children}
+      </Button>
+    );
+  }
+}
+
+export function OrderSmallButton({ children, onClick }) {
+  return (
+    <Button
+      type='button'
+      onClick={onClick}
+      width='130px'
+      height='40px'
+      font='16px'
+      weight='500'
+    >
+      {children}
+    </Button>
+  );
+}
+
+export function OrderBigButton({ children, onClick, disabled }) {
+  if (disabled) {
+    return (
+      <Button
+        type='button'
+        onClick={onClick}
+        width='220px'
+        height='68px'
+        font='24px'
+        backColor='gray'
+      >
+        {children}
+      </Button>
+    );
+  } else {
+    return (
+      <Button
+        type='button'
+        onClick={onClick}
+        width='220px'
+        height='68px'
+        font='24px'
+      >
+        {children}
+      </Button>
+    );
+  }
+}
+
+export function AddressButton({ children, onClick }) {
+  return (
+    <Button
+      type='button'
+      onClick={onClick}
+      width='154px'
+      height='40px'
+      font='16px'
+      weight='500'
+    >
+      {children}
+    </Button>
+  );
+}
+
+export function NotFoundButton({ children, onClick, whiteStyle }) {
+  if (whiteStyle) {
+    return (
+      <Button
+        type='button'
+        onClick={onClick}
+        width='200px'
+        height='60px'
+        backColor='white'
+        fontColor='darkGray'
+        borderColor='gray'
+      >
+        {children}
+      </Button>
+    );
+  } else {
+    return (
+      <Button type='button' onClick={onClick} width='200px' height='60px'>
+        {children}
+      </Button>
+    );
+  }
+}
---
+++ src/components/atoms/button/style.js
@@ -0,0 +1,28 @@
+import styled, { css } from "styled-components";
+import palette from "../../../style/globalColor";
+
+export const Button = styled.button`
+  width: ${(props) => props.width || "480px"};
+  height: ${(props) => props.height || "60px"};
+  font-size: ${(props) => props.font || "18px"};
+  font-weight: ${(props) => props.weight || "700"};
+  color: ${(props) => palette[props.fontColor] || palette.white};
+  background-color: ${(props) => palette[props.backColor] || palette.mainColor};
+  border-radius: 5px;
+
+  ${(props) =>
+    props.borderColor &&
+    css`
+      border: 1px solid ${palette[props.borderColor]};
+    `}
+`;
+
+export const TabButton = styled(Button)`
+  width: 320px;
+  height: 60px;
+  font-size: 18px;
+  background-color: ${palette.white};
+  border-radius: 0;
+  border-bottom: 6px solid ${(props) => palette[props.border]};
+  box-sizing: border-box;
+`;
